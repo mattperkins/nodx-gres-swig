22a86eca10871c7226a479cbaf1d085245f640a9
mattperkins <github@mattperkins.com>
2018-09-28 19:57:31 +0100

    add sql and seed database

--- server.js
+++ server.js
@@ -2,16 +2,32 @@ const pg = require('pg')
 require('dotenv').config()
 
 const client = new pg.Client(process.env.DB_URL)
+const seed = () => {
+ const qry = `
+  DROP TABLE IF EXISTS products;
+  CREATE TABLE products (
+   id SERIAL primary key,
+   name text
+  );
+  insert into products (name) values ('foo');
+  insert into products (name) values ('bar');
+  insert into products (name) values ('baz');
+ `
+ client.query(qry, (err, result) => {
+  if(err){
+   console.log(err)
+  }
+ })
+}
 
-client.connect((err) => {
- if(!err){
-  client.query('select * from foos', (err, result) => {
-   if(err){
-    console.log(err)
+const connect = () => {
+ client.connect((err) => {
+  if(!err){
+   if(process.env.SEED){
+    console.log('Seed DB')
+     seed()
    }
-   else{
-    console.log(result.rows)
-   }
-  })
- }
-})
+  }
+ })
+}
+connect()
