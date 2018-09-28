1829da79d6ec5d43ea510512af4f4db319125114
mattperkins <github@mattperkins.com>
2018-09-28 21:28:25 +0100

    add express server

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
-const db = require('./db')
+const express = require('express')
 
+const db = require('./db')
 db.connect()
+
+const app = express()
+
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log( `Listening on port: ${port}`) )
